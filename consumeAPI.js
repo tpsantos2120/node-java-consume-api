f6b53e046c8d6b1940f74effd73cbede7fd7b752
tpsantos2120 <thiagocroza472@gmail.com>
2021-06-30 14:11:51 -0300

    rename file

---
+++ consumeAPI.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch');
+
+// Example POST method implementation:
+async function handleRequest(method = '', url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: method,
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+
+
+const get = (method, url, data) => {
+    return handleRequest(method, url, data);
+};
+
+const create = (method, url, data) => {
+    return handleRequest(method, url, data);
+};
+
+const update = (method, url, data) => {
+    return handleRequest(method, url, data);
+};
+
+const remove = (method, url, data) => {
+    return handleRequest(method, url, data);
+};
+
+create('POST', 'http://localhost:8080/employees/', { name: "Samwise Gamgee", role: "gardener" })
+    .then(data => {
+        console.log(data);
+    });
